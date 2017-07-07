59714f38740fe5c9f92a311a19a31d0b7e63220b
liaolongdong <924902324@qq.com>
2017-07-07 18:28:26 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import BasicExample from './BasicExample';
+import SidebarExample from './SidebarExample';
+import AnimationExample from './AnimationExample';
+import RouteConfigExample from './RouteConfigExample';
+import ModalGallery from './ModalGallery';
+// import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+ReactDOM.render(<ModalGallery/>, document.getElementById('root'));
+
+// registerServiceWorker();
